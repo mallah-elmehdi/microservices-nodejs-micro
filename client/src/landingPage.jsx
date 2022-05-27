fcd56c166457d8df6d5ed2c163da2129fd20b3be
mallah-elmehdi <elmehdimallah@gmail.com>
2022-05-27 21:02:23 +0100

    updating services

--- client/src/landingPage.jsx
+++ client/src/landingPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Container, Flex, Button } from '@chakra-ui/react';
+import { Container, Flex, Button, Stack } from '@chakra-ui/react';
 import Post from './Post';
 
 export default () => {
 	return (
-		<Container maxW='container.xl' px={0} py={10}>
-			<label>Create a post</label>
-			<Button />
-			<Flex py={20} flexDir="column" >
-				<Post />
-			</Flex>
-		</Container>
+		<Stack>
+			Tex
+				<label>Create a post</label>
+			<Container maxW='container.xl' px={0} py={10}>
+				<Button />
+				<Flex py={20} flexDir='column'>
+					<Post />
+				</Flex>
+			</Container>
+		</Stack>
 	);
 };
