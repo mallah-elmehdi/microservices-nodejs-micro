234d0a59ca51dc5101895d58987956f8fb0b7a58
mallah-elmehdi <elmehdimallah@gmail.com>
2022-05-27 19:42:00 +0100

    updating services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import LandingPage from './landingPage';
 
 export default () => {
 	return (
-		<>
+		<ChakraProvider>
 			<LandingPage />
-		</>
+		</ChakraProvider>
 	);
 };
