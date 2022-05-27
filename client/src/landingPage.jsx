7b61d4b7492879a26f927962f488fda6112d54e7
mallah-elmehdi <elmehdimallah@gmail.com>
2022-05-27 20:22:13 +0100

    updating services

--- client/src/landingPage.jsx
+++ client/src/landingPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Container,Flex, VStack } from '@chakra-ui/react';
+import { Container, Flex, VStack } from '@chakra-ui/react';
 
 export default () => {
 	return (
-		<Container maxW={container.xl} p={0}>
-			<Flex h="100vh" py={20}>
-
+		<Container maxW='container.xl' p={0}>
+			<Flex py={20} flexDir="column" >
+				<VStack
+					w='full'
+					h='full'
+					p={10}
+					spacing={10}
+					bg='gray.100'
+					borderRadius='3xl'></VStack>
+				<VStack
+					w='full'
+					h='full'
+					p={10}
+					spacing={10}
+					bg='gray.100'
+					borderRadius='3xl'></VStack>
 			</Flex>
 		</Container>
 	);
