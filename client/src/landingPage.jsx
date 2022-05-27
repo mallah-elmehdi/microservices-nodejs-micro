81c20384e4dfee95af6f07089c2ff47d32c2ac11
mallah-elmehdi <elmehdimallah@gmail.com>
2022-05-27 19:21:57 +0100

    updating services

--- client/src/landingPage.jsx
+++ client/src/landingPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
 	return (
 		<>
-			<div className='bg-dark min-vh-100 text-white'>hello</div>
+			<div className='bg-dark min-vh-100 text-white'>salam</div>
 		</>
 	);
 };
